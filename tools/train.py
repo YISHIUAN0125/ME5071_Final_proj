450724adb66a4ac983a52c1747a10739c92e9c74
YISHIUAN0125 <t8015nyy3kass@gmail.com>
2025-11-23 15:36:10 +0800

    build DuoSupervision Trainer

--- tools/train.py
+++ tools/train.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO("./models/yolo11n-seg.pt")
-model.to('cuda')
+if __name__ == '__main__':
+    model = YOLO("./models/yolo11n-seg.pt")
+    model.to('cuda')
 
-results = model.train(data="data/data.yaml", batch=8, epochs=100, imgsz=1000, patience=20)
+    results = model.train(data="data/data.yaml", batch=8, epochs=100, imgsz=1000, patience=20)
